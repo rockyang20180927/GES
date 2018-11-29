0cf380017ec046a22ba4b3bffbad7800de3530cc
rockyang20180927 <whiter_nb@hotmail.com>
2018-11-29 16:01:07 +0800

    add the Comm Project

---
+++ Comm/src/main/java/ges/Acc/AccList.java
@@ -0,0 +1,10 @@
+package ges.Acc;
+
+import lombok.Data;
+
+@Data
+public class AccList {
+
+    private  String AccNo;
+    private String AccName;
+}
---
+++ Comm/src/main/java/ges/App.java
@@ -0,0 +1,13 @@
+package ges;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
